575050e0f5338ad5e2bf3e2f2639fd42229b82a1
Rahul Dixit <125376372+alertproof@users.noreply.github.com>
2023-02-15 00:01:51 +0530

    Question 3

---
+++ floorandceil.c
@@ -0,0 +1,12 @@
+// Floor and Ceil
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float num;
+    scanf("%f", &num);
+    printf("%f\n", floor(num));
+    printf("%f", ceil(num));
+    return 0;
+}
