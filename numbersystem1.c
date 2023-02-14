bfd6501a6f0e321ae7109274f4cfd5cc23388d6b
Rahul Dixit <125376372+alertproof@users.noreply.github.com>
2023-02-15 00:01:29 +0530

    Question 2

---
+++ numbersystem1.c
@@ -0,0 +1,13 @@
+// Number System 1
+
+#include <stdio.h>
+
+int main() {
+    int value;
+    scanf("%d", &value);
+    printf("%i\n", value);
+    printf("%o\n", value);
+    printf("%x\n", value);
+    printf("%X", value);
+    return 0;
+}
